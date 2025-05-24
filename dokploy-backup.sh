b03d21721b083a3509900f506dfcc1fa4022e189
scriptPilot <dennis@scheffel.de>
2025-05-24 19:34:51 +0200

    do not stop container for backup

--- dokploy-backup.sh
+++ dokploy-backup.sh
@@ -46,16 +46,6 @@ mkdir -p "$BACKUP_DIR"
 mkdir "$BACKUP_DIR/volumes"
 echo "[INFO] Backup folder created at $BACKUP_DIR"
 
-# Stop all Dokploy services
-echo "[INFO] Stopping Dokploy services..."
-sshpass -p "$BACKUP_SERVER_PW" ssh -o StrictHostKeyChecking=no root@"$BACKUP_SERVER_IP" 'docker service rm dokploy dokploy-postgres dokploy-redis > /dev/null 2>&1 || true'
-echo "[INFO] Dokploy services stopped."
-
-# Stop all running containers
-echo "[INFO] Stopping all running containers..."
-sshpass -p "$BACKUP_SERVER_PW" ssh -o StrictHostKeyChecking=no root@"$BACKUP_SERVER_IP" 'docker ps -q | xargs -r docker stop > /dev/null 2>&1'
-echo "[INFO] All running containers stopped."
-
 # Backup /etc/dokploy folder
 BACKUP_DATE=$(date +"%Y-%m-%d")
 BACKUP_DIR="$LOCAL_BACKUP_DIR/$BACKUP_DATE"
@@ -75,25 +65,6 @@ for volume in $volumes; do
   sshpass -p "$BACKUP_SERVER_PW" scp -o StrictHostKeyChecking=no -q root@"$BACKUP_SERVER_IP":/tmp/$volume.tar.gz "$BACKUP_DIR/volumes/"
   echo "[INFO] Volume $volume archived and downloaded."
 done
-
-# Restart all Dokploy services
-# Use --quiet to suppress noisy logs
-echo "[INFO] Restarting Dokploy services..."
-for service_cmd in \
-  "docker service create --name dokploy-postgres --constraint 'node.role==manager' --network dokploy-network --env POSTGRES_USER=dokploy --env POSTGRES_DB=dokploy --env POSTGRES_PASSWORD=amukds4wi9001583845717ad2 --mount type=volume,source=dokploy-postgres-database,target=/var/lib/postgresql/data postgres:16" \
-  "docker service create --name dokploy-redis --constraint 'node.role==manager' --network dokploy-network --mount type=volume,source=redis-data-volume,target=/data redis:7" \
-  "docker service create --name dokploy --replicas 1 --network dokploy-network --mount type=bind,source=/var/run/docker.sock,target=/var/run/docker.sock --mount type=bind,source=/etc/dokploy,target=/etc/dokploy --mount type=volume,source=dokploy-docker-config,target=/root/.docker --publish published=3000,target=3000,mode=host --update-parallelism 1 --update-order stop-first --constraint 'node.role == manager' -e ADVERTISE_ADDR=$BACKUP_SERVER_IP dokploy/dokploy:latest"
-do
-  sshpass -p "$BACKUP_SERVER_PW" ssh -o StrictHostKeyChecking=no root@"$BACKUP_SERVER_IP" "$service_cmd > /dev/null 2>&1"
-done
-echo "[INFO] Dokploy services restarted."
-
-echo "[INFO] Restarting all containers..."
-stopped_containers=$(sshpass -p "$BACKUP_SERVER_PW" ssh -o StrictHostKeyChecking=no root@"$BACKUP_SERVER_IP" "docker ps -a -q -f status=exited -f status=created")
-if [ -n "$stopped_containers" ]; then
-  sshpass -p "$BACKUP_SERVER_PW" ssh -o StrictHostKeyChecking=no root@"$BACKUP_SERVER_IP" "docker start $stopped_containers" > /dev/null 2>&1
-fi
-echo "[INFO] All containers restarted."
 
 # Log completion
 END_TIME=$(date +%s)
